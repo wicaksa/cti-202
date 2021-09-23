ce0e7a1a195cf3ec36f2e15b9e1d040830f29b02
wicaksa <50036161+wicaksa@users.noreply.github.com>
2021-09-23 15:40:09 -0700

    Create occurences.py

---
+++ occurences.py
@@ -0,0 +1,18 @@
+# Given two lists of numbers, count how many numbers of the first one occur in the second one.
+
+def occurence(A, B):
+   # Counts the occurences of items in list A that's in list B
+      # Convert each to a set
+      A = set(A)
+      B = set(B)
+      
+      # Get the length of the intersection between the two lists
+      print(len(A.intersection(B)))
+      
+def main(): 
+   # Get user input and convert it into two lists 
+   A = [num for num in input().split()]
+   B = [num for num in input().split()]
+
+if __name__ == "__main__":
+   main()
